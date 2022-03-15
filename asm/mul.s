3110f6b3f75c6562acd44dc19164a78650d1b42a
thanmai <thanmai@pdx.edu>
2022-03-15 02:15:43 -0700

    mul instructions testing fike added

---
+++ asm/mul.s
@@ -0,0 +1,15 @@
+
+# MUL
+addi R10, R0, 5     # mul
+addi R11, R0, 11
+mul R12, R10, R11
+
+#MULW
+addi R10, R0, 5     # mulw
+addi R11, R0, 11
+mulw R12, R10, R11
+
+#DIV
+addi R10, R0, 120   # div
+addi R11, R0, 15
+div R12, R10, R11
